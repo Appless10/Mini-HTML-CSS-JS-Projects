68ac7d1845814af118a67456b6490306fa02085b
SageSpikes <boppanajishnu@gmail.com>
2024-07-31 22:28:08 -0400

    Removed rounding from calculation

--- Weight Converter/script.js
+++ Weight Converter/script.js
@@ -1,10 +1,8 @@
 let weightInput;
 let weightOutput;
-let weightOutputRound;
 
 document.getElementById("mySubmit").onclick = function(){
     weightInput = document.getElementById("myText").value;
     weightOutput = (weightInput * 0.45359237);
-    weightOutputRound = Math.round(weightOutput);
-    document.getElementById("kg").innerHTML = weightOutputRound;
+    document.getElementById("kg").innerHTML = weightOutput;
 }
